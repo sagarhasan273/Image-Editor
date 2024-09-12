3bce43f8c6d4b8d4d0a96ee850d0097fff2817a0
sagarhasan273 <sagarhasan273@gmail.com>
2024-09-13 05:28:09 +0600

    image cropper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Box } from '@mui/material';
 import './App.css';
+import ImageEditorCroper from './components/ImageEditorCroper';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Box>
+      <ImageEditorCroper />
+    </Box>
+  );
 }
 
 export default App;
